088dbf132339da5c6f709dd0e7dd2488a463bf61
ysthey <ysthey@gmail.com>
2016-06-23 17:21:14 +0200

    added notification launcher for slave

--- TelepresenceBot/slave/src/main/java/org/makeathon/telepresenceslave/MainActivity.java
+++ TelepresenceBot/slave/src/main/java/org/makeathon/telepresenceslave/MainActivity.java
@@ -3,6 +3,9 @@ package org.makeathon.telepresenceslave;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -83,6 +86,29 @@ public class MainActivity extends AppCompatActivity {
     private String mRobotAddress;
 
     private boolean isPortsConfigured;
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Intent intent = new Intent(this, MainActivity.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) 10003, intent, 0);
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("TelepresenceBot")
+                .setContentText("launch controller")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true).build();
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, n);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
