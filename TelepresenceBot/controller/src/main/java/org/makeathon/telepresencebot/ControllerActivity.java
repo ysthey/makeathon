c1b82c68441721eb923b91433bc018dc27d6ae92
ysthey <ysthey@gmail.com>
2016-06-23 18:10:42 +0200

    remove titile bar

--- TelepresenceBot/controller/src/main/java/org/makeathon/telepresencebot/ControllerActivity.java
+++ TelepresenceBot/controller/src/main/java/org/makeathon/telepresencebot/ControllerActivity.java
@@ -38,6 +38,7 @@ public class ControllerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
