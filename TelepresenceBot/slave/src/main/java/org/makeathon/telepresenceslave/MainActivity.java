0b9715b46b103a4275d161beed960d981f24513e
Hui-Joo Goh <hui-joo.goh@avira.com>
2016-06-23 15:20:22 +0200

    UI & logic for Lego Mindstorm bluetooth pairing & controls

--- TelepresenceBot/slave/src/main/java/org/makeathon/telepresenceslave/MainActivity.java
+++ TelepresenceBot/slave/src/main/java/org/makeathon/telepresenceslave/MainActivity.java
@@ -1,7 +1,10 @@
 package org.makeathon.telepresenceslave;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.makeathon.telepresenceslave.roboliterate.activities.RemoteControlActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, RemoteControlActivity.class));
     }
 }
