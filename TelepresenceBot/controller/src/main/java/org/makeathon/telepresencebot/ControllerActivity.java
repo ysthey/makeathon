a495e0a2364369c60652bdaa633364cd98187fe0
ysthey <ysthey@gmail.com>
2016-06-23 12:37:18 +0200

    added buttons and send signal

--- TelepresenceBot/controller/src/main/java/org/makeathon/telepresencebot/ControllerActivity.java
+++ TelepresenceBot/controller/src/main/java/org/makeathon/telepresencebot/ControllerActivity.java
@@ -2,12 +2,132 @@ package org.makeathon.telepresencebot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.pubnub.api.Callback;
+import com.pubnub.api.Pubnub;
+import com.pubnub.api.PubnubError;
+import com.pubnub.api.PubnubException;
 
 public class ControllerActivity extends AppCompatActivity {
-
+    private Pubnub pubnub;
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+        pubnub = new Pubnub("pub-c-e0e0e558-9aa1-412e-a4ca-ce286e939e54", "sub-c-4b5e362c-27fd-11e6-84f2-02ee2ddab7fe");
+
+    }
+
+    public void forward(View view){
+        pubnub.publish("my_channel", "F", new Callback() {});
+    }
+
+    public void backward(View view){
+        pubnub.publish("my_channel", "B", new Callback() {});
+    }
+    public void left(View view){
+        pubnub.publish("my_channel", "L", new Callback() {});
+
+    }
+    public void right(View view){
+        pubnub.publish("my_channel", "R", new Callback() {});
+
+    }
+
+    public void connect(View view){
+
+        try {
+            pubnub.subscribe("my_channel", new Callback() {
+                        @Override
+                        public void connectCallback(String channel, Object message) {
+                            pubnub.publish("my_channel", "Hello from the PubNub Java SDK", new Callback() {});
+                            pubnub.publish("my_channel", "Hello from the PubNub Java SDK2", new Callback() {});
+
+                        }
+
+                        @Override
+                        public void disconnectCallback(String channel, Object message) {
+                            System.out.println("SUBSCRIBE : DISCONNECT on channel:" + channel
+                                    + " : " + message.getClass() + " : "
+                                    + message.toString());
+                        }
+
+                        public void reconnectCallback(String channel, Object message) {
+                            System.out.println("SUBSCRIBE : RECONNECT on channel:" + channel
+                                    + " : " + message.getClass() + " : "
+                                    + message.toString());
+                        }
+
+                        @Override
+                        public void successCallback(String channel, Object message) {
+                            System.out.println("SUBSCRIBE : " + channel + " : "
+                                    + message.getClass() + " : " + message.toString());
+                        }
+
+                        @Override
+                        public void errorCallback(String channel, PubnubError error) {
+                            System.out.println("SUBSCRIBE : ERROR on channel " + channel
+                                    + " : " + error.toString());
+                        }
+                    }
+            );
+        } catch (PubnubException e) {
+            System.out.println(e.toString());
+        }
+
+    }
+
+    public void disconnect(View view){
+        pubnub.unsubscribe("my_channel", new Callback() {
+            @Override
+            public void connectCallback(String channel, Object message) {
+                System.out.println("UNSUBSCRIBE : DISCONNECT on channel:" + channel
+                        + " : " + message.getClass() + " : "
+                        + message.toString());
+            }
+
+            @Override
+            public void disconnectCallback(String channel, Object message) {
+                System.out.println("UNSUBSCRIBE : DISCONNECT on channel:" + channel
+                        + " : " + message.getClass() + " : "
+                        + message.toString());
+            }
+
+            public void reconnectCallback(String channel, Object message) {
+                System.out.println("UNSUBSCRIBE : RECONNECT on channel:" + channel
+                        + " : " + message.getClass() + " : "
+                        + message.toString());
+            }
+
+            @Override
+            public void successCallback(String channel, Object message) {
+                System.out.println("UNSUBSCRIBE : " + channel + " : "
+                        + message.getClass() + " : " + message.toString());
+            }
+
+            @Override
+            public void errorCallback(String channel, PubnubError error) {
+                System.out.println("UNSUBSCRIBE : ERROR on channel " + channel
+                        + " : " + error.toString());
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroy(){
+        pubnub.shutdown();
+        super.onDestroy();
+
+    }
+
+    public void publish(View view){
+
+
+        pubnub.publish("my_channel", "message " + Integer.toString(counter), new Callback() {});
+        counter ++;
+
     }
 }
